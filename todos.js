5ee2f3d2651fc019a52ed07fc01b4c33376b111a
yeonjukim <kyj5036@hanmail.net>
2015-03-12 18:23:34 +0900

    150312
    실습

--- todos.js
+++ todos.js
@@ -1,2 +1,32 @@
 //start...
-console.log('hello');
+//console.log('hello');
+
+function getDom(id){
+	return document.getElementById(id);
+}
+
+
+//var todoStringField = document.getElementById('todoString');
+var todoStringField = getDom('todoString');
+//var form = document.getElementById('todoForm');
+
+todoStringField.addEventListener('keyup',function() {
+//	console.log(todoStringField,todoStringField.value);
+	if(event.keyCode === 13){
+		console.log(todoStringField.value);
+
+		var newTodo = todoStringField.value;
+		todoStringField.value = "";
+
+		var listDom = getDom('todoList');
+
+		listDom.innerHTML += ' \
+		<li>\
+			<button class = "delete">*</button>\
+			<input type = "checkbox" class="toggle-checked">\
+			<span class = "text"> '+ newTodo +' </span>\
+		</li>';
+	}
+
+});
+//console.log(todoStringField,todoStringField.value);
